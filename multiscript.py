57a793f46dbf81079a84d196be0d704feff5cbe4
Pavel Ovchinnikov <ris3nanderson@gmail.com>
2023-03-17 14:02:08 +0300

    Update

--- multiscript.py
+++ multiscript.py
@@ -3,8 +3,22 @@ import sys
 import threading
 
 thrs = []
+paths = []
 
-for i in range(1, len(sys.argv)):
-    thrs.append(threading.Thread(target=lambda path: os.system(f'python3 {path}'), args=[sys.argv[i]]))
+if len(sys.argv) == 1:
+    if os.path.isfile('.msclist'):
+        with open('.msclist', 'r') as file:
+            paths_ = file.read()
+            paths = paths_.split('\n')[:-1]
+else:
+    paths = sys.argv
+if not paths:
+    raise ValueError('no paths specified')
+
+for i in range(0, len(paths)):
+    if paths[i] == __file__.split('/')[-1]: continue
+    thrs.append(threading.Thread(target=lambda path: os.system(f'python3 {path}'), args=[paths[i]]))
     thrs[-1].start()
-    print(f'- {sys.argv[i]} started')
+    print(f'- {paths[i]} started')
+
+print()
